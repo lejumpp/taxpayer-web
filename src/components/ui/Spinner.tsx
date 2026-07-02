d1eaa1beb7dfc0a4110fed9c4ff9342707640fe8
Nicholas Jumpp <nicholasjumpp1@gmail.com>
2026-07-02 17:32:20 -0500

    ui library

--- src/components/ui/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner({ className = '' }: { className?: string }) {
-  return (
-    <div
-      className={`h-6 w-6 animate-spin rounded-full border-2 border-gray-200 border-t-primary ${className}`}
-    />
-  )
-}
